f10154edefe35312e50c8cdc637b474ed8fe9937
mark <markpackham1@gmail.com>
2020-06-30 11:10:15 +0100

    add @Repository in interfaces

--- src/main/java/com/example/codeclan/employees/repositories/EmployeeRepository.java
+++ src/main/java/com/example/codeclan/employees/repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.codeclan.employees.repositories;
 
 import com.example.codeclan.employees.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
